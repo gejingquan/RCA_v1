0189df7173554f0e5c90c924b0d532fbe089226c
gejingquan <jingquan.ge@ntu.edu.sg>
2023-06-14 20:28:46 +0800

    gejingquan update 8

--- initialization.sh
+++ initialization.sh
@@ -7,4 +7,6 @@ EVAL_DIR=`pwd`
 AFL_DIR=$EVAL_DIR/afl-fuzz
 AFL_WORKDIR=$EVAL_DIR/afl-workdir
 export PIN_ROOT="$(pwd)/pin-3.15-98253-gb56e429b1-gcc-linux"
+export LD_LIBRARY_PATH=~/workspace/out/eblinux/qemu-x86/Fsanitize/all
+
 
